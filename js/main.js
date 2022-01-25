f0747aa319f564b6ebffaf0e9b0afc1a5a13ba15
hochan222 <hochan049@gmail.com>
2022-01-25 18:09:44 +0900

    feat: 순차적 애니메잇견 -전역 버튼 스타일

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
-const searchEl = document.querySelector('.search');
-const seearchInputEl = searchEl.querySelector('input');
+const searchEl = document.querySelector(".search");
+const seearchInputEl = searchEl.querySelector("input");
 
-searchEl.addEventListener('click', function () {
-    seearchInputEl.focus();
-})
+searchEl.addEventListener("click", function () {
+  seearchInputEl.focus();
+});
 
-seearchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    seearchInputEl.setAttribute('placeholer', '통합검색');
-})
+seearchInputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused");
+  seearchInputEl.setAttribute("placeholer", "통합검색");
+});
 
-seearchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    seearchInputEl.setAttribute('placeholer', '');
-})
+seearchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  seearchInputEl.setAttribute("placeholer", "");
+});
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
